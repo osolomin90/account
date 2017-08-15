61721b5176138f9ff7a5e6e8f5e407920bca53ea
DanteGrek <solomin60@gmail.com>
2017-08-15 17:39:28 +0300

    added module for unittest

---
+++ account_unittests.py
@@ -0,0 +1,11 @@
+import unittest
+from account import Transactions
+
+
+class AccountUnittests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_read_transactions_from_transactions_txt(self):
+        pass
