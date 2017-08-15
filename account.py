6f1e720f80b20b104b52a789398defecd141c366
Dante <osolomin@corevalue.net>
2017-08-16 01:20:20 +0300

    update 'account' and added tests

--- account.py
+++ account.py
@@ -1,30 +1,48 @@
 class Transactions:
 
     def __init__(self):
-        self.transactions = []
+        self._transactions = []
+        self._file_read = False
+
+    def _print_message(self):
+        print "Pleas create file 'transactions.txt' in format 'mm-dd-yyyy Type $xx.xx' and try again."
 
     def read_transactions(self, txt_file='transactions.txt'):
-        for t in open(txt_file):
-            self.transactions.append(t)
-        return self
+        try:
+            for t in open(txt_file):
+                if "$" and "-" in str(t) and str(t).split(" ").__len__() == 3:
+                    self._transactions.append(t)
+                else:
+                    return self
+            self._file_read = True
+        except IOError:
+            pass
+        finally:
+            return self
 
     def get_transactions_size(self):
-        return self.transactions.__len__()
+        if self._file_read:
+            return self._transactions.__len__()
 
     def get_total_balance(self):
-        total = 0
-        for t in self.transactions:
-            total += float(t.split('$')[1].rstrip())
-        return total
+        if self._file_read:
+            total = 0
+            for t in self._transactions:
+                total += float(t.split('$')[1].rstrip())
+            return total
 
     def get_first_transaction_with_negative_balance(self):
-        for t in self.transactions:
-            if '-' in t.split('$')[1].rstrip():
-                return t
-
+        if self._file_read:
+                for t in self._transactions:
+                    if '-' in t.split(' ')[2].rstrip():
+                        return t.split(' ')[0].rstrip()+" "+t.split(' ')[2].rstrip()
 
 if __name__ == '__main__':
     transactions = Transactions().read_transactions()
-    print "Total:", transactions.get_transactions_size()
-    print "Total of balance:", transactions.get_total_balance()
-    print "First transaction with negative balance: ", transactions.get_first_transaction_with_negative_balance()
+
+    if transactions._file_read:
+        print "Total:", transactions.get_transactions_size()
+        print "Total of balance:", transactions.get_total_balance()
+        print "First transaction with negative balance: ", transactions.get_first_transaction_with_negative_balance()
+    else:
+        transactions._print_message()
--- account_unittests.py
+++ account_unittests.py
@@ -1,14 +1,51 @@
 import unittest
+import os
 from account import Transactions
 
 
 class AccountUnittests(unittest.TestCase):
 
+    __test_file = "test.txt"
+
     def setUp(self):
-        pass
+         self.file = open(self.__test_file, "w")
 
     def test_read_transactions_from_transactions_txt(self):
-        assert Transactions().read_transactions('transactions.txt').transactions.__len__() > 0
+        with self.file as f:
+            f.write("01-02-2017 Deposit $4000.00")
+        assert Transactions().read_transactions(self.__test_file)._transactions.__len__() > 0,\
+            "File is empty or system can not read 'transaction.txt'"
+
+    def test_is_transaction_count_correct(self):
+        with self.file as f:
+            f.write("01-02-2017 Deposit $4000.00\n")
+            f.write("01-02-2017 Deposit $-1000.00")
+        assert Transactions().read_transactions(self.__test_file).get_transactions_size() == 2
+
+    def test_total_balance(self):
+        with self.file as f:
+            f.write("01-02-2017 Deposit $4000.00\n")
+            f.write("01-02-2017 Deposit $-1000.00\n")
+            f.write("01-02-2017 Deposit $150.55")
+        assert Transactions().read_transactions(self.__test_file).get_total_balance() == 3150.55
+
+    def test_first_transaction_with_negative_balance(self):
+        with self.file as f:
+            f.write("01-02-2017 Deposit $4000.00\n")
+            f.write("01-02-2017 Deposit $-1000.00\n")
+            f.write("01-02-2017 Deposit $150.55\n")
+            f.write("01-02-2017 Deposit $-2150.55")
+        assert "-1000" and "01-02-2017" in Transactions().read_transactions(self.__test_file)\
+                   .get_first_transaction_with_negative_balance()
+
+    def test_if_transaction_format_incorrect(self):
+        with self.file as f:
+            f.write("$-4000.00\n")
+        assert Transactions().read_transactions(self.__test_file) \
+            .get_first_transaction_with_negative_balance() is None
+
+    def tearDown(self):
+        os.remove(self.__test_file)
 
 if __name__ == '__main__':
     unittest.main()
